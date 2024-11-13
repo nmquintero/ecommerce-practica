f8b8b9597980348602d908718e635e0430343456
Nicolas <108997865+nmquintero@users.noreply.github.com>
2024-11-12 22:07:34 -0500

    new details

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,13 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
 export const ShoppingCardContext = createContext();
 
 export const ShoppingCardProvider = ({children})=>{
     const [count, setCount] = useState(0)
+    const [isDetailOpem, setIsDetailOpem] = useState(false)
     return(
         <ShoppingCardContext.Provider value={{
             count,
-            setCount
+            setCount,
+            isDetailOpem,
+            setIsDetailOpem
         }}>
             {children}
         </ShoppingCardContext.Provider>
